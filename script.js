320a8dd0c2f72e32fdfccad294d8b853eafb56b2
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-07-22 22:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+	//Write your code here
+	let table = document.getElementById('sampleTable');
+    let row = table.insertRow(0);
+
+    let leftCell = row.insertCell(0);
+    let rightCell = row.insertCell(1);
+    leftCell.innerHTML = 'New Cell1';
+    rightCell.innerHTML = 'New Cell2';
 }
